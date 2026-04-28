04fce6aa33a6b9ea25fe73448ee601f2c034099f
Felix Steiner <felix.steiner@stud.hslu.ch>
2026-04-28 21:56:40 +0200

    - experimented with litellm - added context and histroy json files as to test context interaction

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
+# IMPORTS
 from litellm import completion
 from dotenv import load_dotenv
+import json
 
+# .ENV
 load_dotenv()
 
+# LOAD JSON DATA
+with open("data/history.json", "r") as f:
+    history = json.load(f)
+with open("data/context.json", "r") as f:
+    context = json.load(f)
+
+# PROMPT
 response = completion(
     model="gemini/gemini-2.5-flash",
-    messages=[{"role": "user", "content": "Hello, how are you?"}]
+    messages=[
+        {
+            "role": "system",
+            "content": f"""
+            Your role:
+            You are a helpfull assistant.
+            Current context:
+            {json.dumps(context, indent=2)}
+            Conversation history:
+            {json.dumps(history, indent=2)}
+            """
+        },
+        {   
+            "role": "user", 
+            "content": "Hello, how are you? Tell me about me. And whats the wheater?"
+         }
+    ]
 )
 
+# PRINT ANSWER
 print(response.choices[0].message.content)
